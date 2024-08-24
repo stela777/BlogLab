d38e479beac92df0d42a6b77ae3cad18a73ddb59
Estela <estelac@lagcc.cuny.edu>
2024-08-23 22:47:15 -0400

    Change demo

--- BlogLab.Web/obj/Debug/netcoreapp3.1/BlogLab.Web.AssemblyInfo.cs
+++ BlogLab.Web/obj/Debug/netcoreapp3.1/BlogLab.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogLab.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759782d0823fd92a3a214d3ec634a56297596488")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogLab.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogLab.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
