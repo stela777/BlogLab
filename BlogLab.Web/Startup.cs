e4e0b069c3b25d9c4f1ecc85f0b45f17b7ed6b3c
Estela <estelac@lagcc.cuny.edu>
2024-08-25 17:33:33 -0400

    Service Project Completed.

--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,3 +1,4 @@
+using BlogLab.Models.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +13,7 @@ using System.Threading.Tasks;
 namespace BlogLab.Web
 {
     public class Startup
-    {
+    { 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,6 +25,7 @@ namespace BlogLab.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
